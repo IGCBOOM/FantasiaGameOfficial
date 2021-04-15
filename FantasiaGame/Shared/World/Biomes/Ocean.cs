0a49e5d60f78f25dee4d84aae1d2cb2a7d01ada8
Asher Simpson <asher.simpson@outlook.com>
2021-04-15 14:49:32 -0400

    Added ocean biomes.

---
+++ FantasiaGame/Shared/World/Biomes/Ocean.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FantasiaAPI.Shared.Worlds.Generation.Biomes;
+
+namespace FantasiaGame.Shared.World.Biomes
+{
+    public class Ocean : Biome
+    {
+
+        public override string ID { get; } = "ocean";
+        public override string BiomeType { get; } = "terra";
+        public override double Height { get; } = 0;
+        public override double Humidity { get; } = 50; // This is for biome placement, not literal humidity!
+        public override double Temperature { get; } = 23;
+        public override Color BiomeColor { get; } = Color.Blue;
+
+    }
+
+}
