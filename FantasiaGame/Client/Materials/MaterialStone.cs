4c86866aec528631bb38e615e62397be87546721
IGCBOOM <caya1.carter1@gmail.com>
2021-04-15 13:00:22 -0500

    Add some blocks, update API.

---
+++ FantasiaGame/Client/Materials/MaterialStone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FantasiaAPI.Client.Renderer;
+
+namespace FantasiaGame.Client.Materials
+{
+
+    public class MaterialStone : Material
+    {
+
+        public override string ID { get; } = "stone";
+
+        public override string TexturePath { get; } = "fantasia/stone.png";
+
+    }
+
+}
