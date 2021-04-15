8f99d7d25bedc7afd2b5ec7632388b3cab98dc98
Asher Simpson <asher.simpson@outlook.com>
2021-04-15 14:02:00 -0400

    Forgot coastal desert.

---
+++ FantasiaGame/World/Biomes/CoastalDesert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FantasiaAPI.Shared.Worlds.Generation.Biomes;
+
+namespace FantasiaGame.World.Biomes
+{
+    public class CoastalDesert : Biome
+    {
+
+        public override string ID { get; } = "coastal_desert";
+        public override string BiomeType { get; } = "terra";
+        public override double Height { get; } = 5.5;
+        public override double Humidity { get; } = 40;
+        public override double Temperature { get; } = 27.5;
+        public override Color BiomeColor { get; } = Color.OliveDrab;
+
+    }
+
+}
